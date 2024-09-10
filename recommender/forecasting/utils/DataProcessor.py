ffd71d389ef377e8b57991da892a3ef7289c53a6
Jesus Camacho Rodriguez <jesusca@microsoft.com>
2024-09-10 15:24:26 -0700

    Add LICENSE text to files (pre-commit hook and actions)

--- recommender/forecasting/utils/DataProcessor.py
+++ recommender/forecasting/utils/DataProcessor.py
@@ -1,3 +1,10 @@
+#
+# --------------------------------------------------------------------------
+#  Licensed under the MIT License. See LICENSE file in the project root for
+#  license information.
+#  Copyright (c) Microsoft Corporation.
+# --------------------------------------------------------------------------
+#
 import warnings
 import time
 
