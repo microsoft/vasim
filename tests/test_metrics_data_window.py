dbb692f6d646fd2f543b397400870d0f79aac471
Jesús Camacho Rodríguez <jesusca@microsoft.com>
2024-09-10 15:41:43 -0700

    Test failure

--- tests/test_metrics_data_window.py
+++ tests/test_metrics_data_window.py
@@ -3,7 +3,7 @@
 #  Licensed under the MIT License. See LICENSE file in the project root for
 #  license information.
 #  Copyright (c) Microsoft Corporation.
-# --------------------------------------------------------------------------
+# --------------------------------------------------------------------
 #
 import unittest
 import os
