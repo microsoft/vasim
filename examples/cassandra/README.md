fed61c56c0d72897393c1a585042f1c14871e397
Karla Saur <kasaur@microsoft.com>
2024-09-13 00:43:11 +0000

    getting started with cassandra demo

---
+++ examples/cassandra/README.md
@@ -0,0 +1,10 @@
+# Cassandra Demo
+
+This is going to be an autoscaling demo of Cassandra.  Currently it is a work-in-progress!!
+
+
+#### Setup:
+
+You must install the python docker package.
+
+`pip install docker`
---
+++ examples/cassandra/poll_metrics.py
@@ -0,0 +1,78 @@
+#
+# --------------------------------------------------------------------------
+#  Licensed under the MIT License. See LICENSE file in the project root for
+#  license information.
+#  Copyright (c) Microsoft Corporation.
+# --------------------------------------------------------------------------
+#
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+import docker
+
+"""
+This script is used to read live CPU usage data from the container and write it to a file.
+
+"""
+
+WAIT_INTERVAL = 60  # how often to poll the CPU usage
+CONTAINER_NAME = "some-cassandra1"  # the container to monitor
+
+
+def get_curr_cpu_usage(container_name=CONTAINER_NAME):
+    """
+    This demo shows only a standalone container, but it could be modified to read CPU usage from a
+        Kubernetes cluster/metrics-server, or from multiple containers.
+    """
+
+    client = docker.from_env()
+    container = client.containers.get(container_name)
+    stats = container.stats(stream=False)
+
+    cpu_delta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
+    system_cpu_delta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
+    number_cpus = len(stats['cpu_stats']['cpu_usage']['percpu_usage'])
+    cpu_percentage = (cpu_delta / system_cpu_delta) * number_cpus
+    return cpu_percentage
+
+
+def get_timestamp():
+    """
+    This is based on the current format that VASim read the timestamps.
+
+    Ideally, this would be replaced with a more robust timestamping mechanism and standard format.
+    See: https://github.com/microsoft/vasim/issues/34
+
+    """
+    return datetime.now().strftime("%Y.%m.%d-%H:%M:%S:%f")
+
+
+if __name__ == "__main__":
+
+    curr_dir = Path().absolute()
+    # if there is no data directory, create it
+    if not Path(str(curr_dir) + "/data").exists():
+        Path(str(curr_dir) + "/data").mkdir()
+    filename = str(curr_dir) + "/data/" + \
+        datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_perf_event_log.csv"
+    monitor_file = Path(filename)
+    if not monitor_file.exists():  # write the header
+        f = open(monitor_file, 'a')
+        f.write("TIMESTAMP,CPU_USAGE_ACTUAL\n")
+    else:
+        f = open(monitor_file, 'a')
+
+    print("starting monitor loop. Writing to " + filename + ". Ctrl-C to exit.")
+    while True:
+        try:
+            usage = get_curr_cpu_usage()
+            readings = "{},{}\n".format(get_timestamp(), usage)
+        except KeyError:
+            print("error getting data (possible update in-flight), trying again")
+            sleep(5)
+            continue
+
+        print(readings)
+        f.write(readings)
+        f.flush()
+        sleep(WAIT_INTERVAL)
