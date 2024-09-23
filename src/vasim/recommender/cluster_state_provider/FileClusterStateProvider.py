a5f9d65673b81a72d2f5e5f03e527bf252533a98
Karla Saur <kasaur@microsoft.com>
2024-09-23 00:22:49 +0000

    Revert "reseting files in main branch"
    This reverts commit cd11de5c0fca645b6077e98f1379e95b83f9beb8.

--- src/vasim/recommender/cluster_state_provider/FileClusterStateProvider.py
+++ src/vasim/recommender/cluster_state_provider/FileClusterStateProvider.py
@@ -10,18 +10,15 @@ import os
 from datetime import datetime, timedelta
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
-from vasim.commons.utils import list_perf_event_log_files
-from vasim.recommender.cluster_state_provider.ClusterStateProvider import (
-    ClusterStateProvider,
-)
+from vasim.recommender.cluster_state_provider.ClusterStateProvider import ClusterStateProvider
 
 
 class FileClusterStateProvider(ClusterStateProvider):
     """
     This class requires a Kubernetes cluster to run.
-
     It reads data from the cluster and uses it to make decisions.
 
     It is not part of the simulator, but is used in the production environment.
@@ -31,48 +28,33 @@ class FileClusterStateProvider(ClusterStateProvider):
     ability to run the simulator with a live k8s cluster.
     """
 
-    def __init__(
-        self,
-        data_dir=None,
-        features=None,
-        window=40,
-        decision_file_path=None,
-        lag=5.0,
-        min_cpu_limit=1,  # pylint: disable=unused-argument   # FIXME
-        max_cpu_limit=None,  # pylint: disable=unused-argument   # FIXME
-        save_metadata=True,
-        **kwargs,
-    ):
-        # pylint: disable=too-many-arguments
+    def __init__(self, data_dir=None, features=["cpu"], window=40, decision_file_path=None, lag=5.0,
+                 granularity=1, min_cpu_limit=1, max_cpu_limit=None, save_metadata=True, **kwargs):
         """
         Parameters:
-
             data_dir (str): The directory where the csvs are stored.
             features (list): The features to use for the model. Currently always ['cpu']. TODO: memory
             window (int): Window in minutes to capture VALID data in order to evaluate PP curve
             decision_file_path (str): The path to the file where Doppler will write decisions
             lag (float): Number of minutes to wait after making a decision
+            granularity (int): Granularity desired for the price-performance curve, formerly "coredelta"
             min_cpu_limit (int): The minimum number of cores to recommend (When set to None it assumes 1)
             max_cpu_limit (int): The maximum number of cores to recommend. (When set to None, it assumes max on the machine)
             save_metadata (bool): Whether to save metadata to a file in the data_dir
         """
-        if features is None:
-            features = ["cpu"]
 
-        if "config" in kwargs:
+        if 'config' in kwargs:
             # keyword argument 'param_name' exists
-            self.config = kwargs["config"]
+            self.config = kwargs['config']
 
         self.logger = logging.getLogger()
 
         self.data_dir = Path(data_dir) or Path().absolute() / "data"
-        if not list_perf_event_log_files(self.data_dir):
-            self.logger.error("Error: no csvs found in data_dir %s", self.data_dir)
-            raise SystemExit(f"Error: no csvs found in data_dir {self.data_dir}")
-
+        if list(self.data_dir.glob("**/*.csv")) == []:
+            self.logger.error('Error: no csvs found in data_dir {}'.format(self.data_dir))
+            raise SystemExit('Error: no csvs found in data_dir {}'.format(self.data_dir))
         # TODO: rename this, it's a bit confusing. It's not the features of the model, it's the features of the data.
         self.features = features or []
-        # TODO: a lot of the code below needs testing
         self.decision_file_path = decision_file_path or "data/decisions.txt"
         self.save_metadata = save_metadata
 
@@ -81,8 +63,9 @@ class FileClusterStateProvider(ClusterStateProvider):
         TODO: Here is where we called into the live system (Kubernetes) to get the current limit of cores being used.
         This is not needed in the simulator, but we leave this placeholder here in case others want to use this code
         to run the simulator with a live k8s cluster.
-        ---
 
+        Returns
+        -------
         cores : int
             The number of cores currently being used.
         """
@@ -91,9 +74,11 @@ class FileClusterStateProvider(ClusterStateProvider):
         # Verify that csvs exist in the data_dir
         # TODO: This is not a scalable way to check for csvs
         # if the data is large, this will take a long time. Need to chunk.
-        csv_paths = list_perf_event_log_files(self.data_dir)
-        if not csv_paths:
-            self.logger.error("Error reading csvs from %s", self.data_dir)
+        csv_paths = list(self.data_dir.glob("**/*.csv"))
+        if csv_paths == []:
+            self.logger.error(f'Error reading csvs from {self.data_dir}')
+
+            print("Error reading csvs")
             return None, None
 
         # Process data
@@ -139,30 +124,36 @@ class FileClusterStateProvider(ClusterStateProvider):
         if recorded_data.shape[0] > 2:
             # Filter out any data points that are greater than the machine max
             # This is because sometimes the telemetry can be incorrect
-            cores = self.get_current_cpu_limit()
-            if cores is None:
-                self.logger.error("Error getting current cores. Retry later.")
-                return None, None
-            recorded_data = recorded_data[recorded_data["cpu"] <= self.config.general_config["max_cpu_limit"]]
+            recorded_data = recorded_data[recorded_data["cpu"] <= self.config.general_config['max_cpu_limit']]
 
         return recorded_data, end_time
 
-    def process_data(self, data=None):
-        assert data is not None, "No data provided to process"
-        assert isinstance(data, list), "Data must be a list"
-        csv_paths = data
+    def process_data(self, csv_paths):
         recorded_data = pd.DataFrame()
-        for csv_path in csv_paths:
-            path = str(csv_path)
-            try:
-                temp_data = pd.read_csv(path)
-                temp_data["cpu"] = temp_data["CPU_USAGE_ACTUAL"]
-                temp_data["time"] = temp_data["TIMESTAMP"].apply(lambda x: datetime.strptime(x, "%Y.%m.%d-%H:%M:%S:%f"))
-                temp_data = temp_data[["time"] + self.features]
-                recorded_data = pd.concat([recorded_data, temp_data], axis=0)
-            except Exception as e:  # pylint: disable=broad-exception-caught  # FIXME
-                self.logger.error("Error reading %s", path, exc_info=e)
-                continue
+        for i, path in enumerate(csv_paths):
+            path = str(path)
+            # The CSVs must have the word "perf_event_log" in them, else we ignore
+            # This is to prevent accidentally reading other CSVs in the directory
+            if "perf_event_log" in path:
+                try:
+                    temp_data = pd.read_csv(path)
+                    temp_data["cpu"] = temp_data["CPU_USAGE_ACTUAL"]
+                    temp_data["time"] = temp_data["TIMESTAMP"].apply(
+                        lambda x: datetime.strptime(x, "%Y.%m.%d-%H:%M:%S:%f"))
+                    temp_data = temp_data[["time"] + self.features]
+                    recorded_data = pd.concat([recorded_data, temp_data], axis=0)
+                except Exception as e:
+                    self.logger.error(f"Error reading {path} {e}")
+                    continue
+        return recorded_data
+
+    def drop_duplicates(self, recorded_data):
+        recorded_data = recorded_data.drop_duplicates()
+        return recorded_data
+
+    def sort_data(self, recorded_data):
+        recorded_data = recorded_data.assign(time=pd.to_datetime(recorded_data["time"]))
+        recorded_data = recorded_data.sort_values(by="time")
         return recorded_data
 
     def truncate_data(self, recorded_data, last_decision_time):
@@ -171,18 +162,32 @@ class FileClusterStateProvider(ClusterStateProvider):
         num_observations = recorded_data.shape[0]
         if num_observations > 2:
             if last_decision_time > end_time:
-                self.logger.warning("Last decision time is greater than end time so avoid making a decision")
+                self.logger.warning(
+                    "Last decision time is greater than end time so avoid making a decision"
+                )
                 return None, None
 
-        start_time = end_time - timedelta(minutes=self.config.general_config["window"])
+        start_time = end_time - timedelta(minutes=self.config.general_config['window'])
         recorded_data = recorded_data[recorded_data["time"] >= start_time]
         recorded_data = recorded_data[recorded_data["time"] <= end_time]
         return recorded_data, end_time
 
+    def create_resource_limits(self):
+        # Infact I think we need to remove most of this file, as it won't be used in the simulator.
+        vcpus_increments = self.config.general_config['max_cpu_limit'] * self.config.general_config['granularity'] + 1
+        new_resource_limits = pd.DataFrame(
+            data={"cpu": np.linspace(0, self.config.general_config['max_cpu_limit'], num=vcpus_increments)})
+        new_resource_limits[
+            "new_price"] = new_resource_limits["cpu"]  # we removed price from the model
+        # TODO: where is this used?
+        new_resource_limits["sku"] = new_resource_limits["cpu"].apply(
+            lambda x: str(round(x, 2)) + "vcpu")
+        return new_resource_limits
+
     def get_last_decision_time(self, recorded_data):
         # Check if the decisions file exists
         if not os.path.exists(self.decision_file_path):
-            self.logger.warning("No decisions file found!  Path was: %s", self.decision_file_path)
+            self.logger.warning("No decisions file found!  Path was: {}".format(self.decision_file_path))
             last_decision_time = recorded_data["time"].iloc[0]
         else:
             # Read in the decisions txt file to see when the last decision was made
@@ -199,15 +204,16 @@ class FileClusterStateProvider(ClusterStateProvider):
                 last_decision_time = recorded_data["time"].iloc[0]
             else:
                 # Identify the time point when the CURR_LIMIT changes (when a decision was enforced)
-                new_limit_changes = decisions[decisions["CURR_LIMIT"].shift(1) != decisions["CURR_LIMIT"]]
+                new_limit_changes = decisions[
+                    decisions["CURR_LIMIT"].shift(1) != decisions["CURR_LIMIT"]]
 
                 # Get the last time a decision was made
                 last_decision_time = new_limit_changes["LATEST_TIME"].iloc[-1]
                 # Add the lag to the last decision time
-                last_decision_time = last_decision_time + timedelta(minutes=self.config.general_config["lag"])
+                last_decision_time = last_decision_time + timedelta(minutes=self.config.general_config['lag'])
 
         return last_decision_time
 
     def get_total_cpu(self):
         # TODO: this function makes less sense in the context of the simulator
-        return self.config.general_config["max_cpu_limit"]
+        return self.config.general_config['max_cpu_limit']
--- src/vasim/simulator/InMemorySimulator.py
+++ src/vasim/simulator/InMemorySimulator.py
@@ -9,25 +9,19 @@ import argparse
 import json
 import logging
 import os
+import uuid
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 
-from vasim.recommender.cluster_state_provider.ClusterStateConfig import (
-    ClusterStateConfig,
-)
 from vasim.recommender.DummyAdditiveRecommender import SimpleAdditiveRecommender
 from vasim.recommender.DummyMultiplierRecommender import SimpleMultiplierRecommender
-from vasim.simulator.analysis.plot_utils import (
-    calculate_and_return_metrics_to_target,
-    plot_cpu_usage_and_new_limit_plotnine,
-)
-from vasim.simulator.ParameterTuning import create_uuid
-from vasim.simulator.SimulatedClusterStateProviderFactory import (
-    SimulatedClusterStateProviderFactory,
-)
+from vasim.recommender.cluster_state_provider.ClusterStateConfig import ClusterStateConfig
 from vasim.simulator.SimulatedInfraScaler import SimulatedInfraScaler
+from vasim.simulator.SimulatedClusterStateProviderFactory import SimulatedClusterStateProviderFactory
+from vasim.simulator.analysis.plot_utils import plot_cpu_usage_and_new_limit_plotnine, calculate_and_return_metrics_to_target
+from vasim.simulator.ParameterTuning import create_uuid
 
 
 class InMemoryRunnerSimulator:
@@ -37,24 +31,11 @@ class InMemoryRunnerSimulator:
     It contains the run method that simulates the cluster state and runs the recommender algorithm.
     """
 
-    # pylint: disable=too-many-instance-attributes
-
-    def __init__(
-        self,
-        data_dir,
-        config_path=None,
-        initial_cpu_limit=None,
-        algorithm="multiplicative",
-        config=None,
-        target_simulation_dir=None,
-        if_resample=True,
-    ):
-        # pylint: disable=too-many-arguments
+    def __init__(self, data_dir, config_path=None, initial_cpu_limit=None, lag=None, algorithm='multiplicative',
+                 config=None, target_simulation_dir=None, if_resample=True):
         worker_id = create_uuid()
         target_simulation_dir = target_simulation_dir or os.path.join(
-            f"{data_dir}_simulations",
-            f"target_{worker_id}",
-        )  # TODO: remove hardcode
+            f"{data_dir}_simulations", f"target_{worker_id}")  # TODO: remove hardcode
         # Create the directory if it doesn't exist
         os.makedirs(target_simulation_dir, exist_ok=True)
 
@@ -75,10 +56,9 @@ class InMemoryRunnerSimulator:
 
         self.out_file = self._initialize_output_file(target_simulation_dir or data_dir)
 
-        self.sleep_interval_minutes = self.config.general_config["lag"]
+        self._configure_sleep_interval(lag, self.config)
 
-    @staticmethod
-    def _setup_logger(data_dir):
+    def _setup_logger(self, data_dir):
         logger = logging.getLogger()
         logger.setLevel(logging.ERROR)
 
@@ -86,62 +66,57 @@ class InMemoryRunnerSimulator:
         file_handler = logging.FileHandler(log_file)
         file_handler.setLevel(logging.WARNING)
 
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
         return logger
 
-    @staticmethod
-    def _load_config(config_path):
+    def _load_config(self, config_path):
         return ClusterStateConfig(filename=config_path)
 
-    @staticmethod
-    def _create_cluster_state_provider(data_dir, config, target_simulation_dir=None):
+    def _create_cluster_state_provider(self, data_dir, config, target_simulation_dir=None):
         out_filename = f"{target_simulation_dir or data_dir}/decisions.txt"  # TODO: remove hardcode. ALSO: todo, this is csv
-        return SimulatedClusterStateProviderFactory(
-            data_dir=data_dir,
-            out_filename=out_filename,
-            config=config,
-        ).create_provider(predictive=config.prediction_config)
+        return SimulatedClusterStateProviderFactory(data_dir=data_dir, out_filename=out_filename, config=config).create_provider(
+            predictive=config.prediction_config)
 
     def _get_experiment_time_range(self):
         return self.cluster_state_provider.start_time, self.cluster_state_provider.end_time
 
     def _create_infra_scaler(self):
-        return SimulatedInfraScaler(
-            self.cluster_state_provider,
-            self.experiment_start_time,
-            self.config.general_config.get("recovery_time", 15),
-        )
+        return SimulatedInfraScaler(self.cluster_state_provider, self.experiment_start_time,
+                                    self.config.general_config.get("recovery_time", 15))
 
     def _create_recommender_algorithm(self, algorithm):
-        if algorithm == "multiplicative":
+        if algorithm == 'multiplicative':
             return SimpleMultiplierRecommender(self.cluster_state_provider)
-        elif algorithm == "additive":
+        elif algorithm == 'additive':
             return SimpleAdditiveRecommender(self.cluster_state_provider)
         # Add your own algorithm here!!!
         # TODO: Make this more dynamic
         else:
             raise ValueError(f"Unknown algorithm: {algorithm}")
 
-    @staticmethod
-    def _initialize_output_file(data_dir):
+    def _initialize_output_file(self, data_dir):
         out_filename = f"{data_dir}/decisions.txt"
         out_file = Path(out_filename)
 
         if not out_file.exists():
-            f = open(out_file, "a", encoding="utf-8")
-            f.write("LATEST_TIME,CURR_LIMIT,NEW_LIMIT\n")
+            f = open(out_file, 'a')
+            f.write("{},{},{}\n".format("LATEST_TIME", "CURR_LIMIT", "NEW_LIMIT"))
             f.flush()
         else:
-            f = open(out_file, "a", encoding="utf-8")
+            f = open(out_file, 'a')
 
         return f
 
+    def _configure_sleep_interval(self, lag, config):
+        # TODO: there may be some double-storing of the lag parameter here
+        self.sleep_interval_minutes = lag or config.general_config['lag']
+
     def output_decision(self, latest_time, current_limit, new_limit):
         if latest_time is not None:
-            to_write = f"{latest_time},{current_limit},{new_limit}\n"
+            to_write = "{},{},{}\n".format(latest_time, current_limit, new_limit)
             self.out_file.write(to_write)
             self.out_file.flush()
         else:
@@ -156,28 +131,26 @@ class InMemoryRunnerSimulator:
                 metrics[key] = int(value)
 
         #        save metrics to file
-        if save_to_file and metrics:
-            with open(f"{self.target_simulation_dir}/calc_metrics.json", "w", encoding="utf-8") as f:
+        if save_to_file and metrics != {}:
+            with open(f"{self.target_simulation_dir}/calc_metrics.json", 'w') as f:
                 json.dump(metrics, f)
             self.cluster_state_provider.config.to_json(f"{self.target_simulation_dir}/metadata.json")
-            plot_cpu_usage_and_new_limit_plotnine(
-                self.cluster_state_provider.data_dir,
-                decision_file_path=f"{self.target_simulation_dir}/decisions.txt",
-                if_resample=self.if_resample,
-            )
+            plot_cpu_usage_and_new_limit_plotnine(self.cluster_state_provider.data_dir,
+                                                  decision_file_path=f"{self.target_simulation_dir}/decisions.txt",
+                                                  if_resample=self.if_resample)
         return metrics
 
-    def run_simulation(self):
-        """Run the simulation to completion and return the final metrics."""
+    def run_simulation(self, save_to_file=True):
+        """
+        Run the simulation to completion and return the final metrics.
+        """
 
         print(f"Starting simulation at {self.experiment_start_time} and continuing till {self.experiment_end_time}")
         print(f"Setting number of cores to {self.initial_cpu_limit}")
         self.cluster_state_provider.set_cpu_limit(self.initial_cpu_limit)
 
-        while (
-            self.cluster_state_provider.current_time + pd.Timedelta(minutes=self.sleep_interval_minutes)
-            < self.cluster_state_provider.end_time
-        ):
+        while self.cluster_state_provider.current_time + pd.Timedelta(
+                minutes=self.sleep_interval_minutes) < self.cluster_state_provider.end_time:
 
             # Core simulation logic (without yielding progress)
             self._execute_simulation_step()
@@ -186,10 +159,12 @@ class InMemoryRunnerSimulator:
         self.cluster_state_provider.flush_metrics_data(f"{self.target_simulation_dir}/perf_event_log.csv")
 
         # Return the final metrics
-        return self.get_metrics()
+        return self.get_metrics(save_to_file=save_to_file)
 
     def run_simulation_with_progress(self):
-        """Run the simulation, yielding progress updates as the simulation progresses, followed by the final result."""
+        """
+        Run the simulation, yielding progress updates as the simulation progresses, followed by the final result.
+        """
         print(f"Starting simulation at {self.experiment_start_time} and continuing till {self.experiment_end_time}")
         print(f"Setting number of cores to {self.initial_cpu_limit}")
         self.cluster_state_provider.set_cpu_limit(self.initial_cpu_limit)
@@ -197,10 +172,8 @@ class InMemoryRunnerSimulator:
         total_time = self.cluster_state_provider.end_time - self.cluster_state_provider.current_time
         time_elapsed = pd.Timedelta(minutes=0)
 
-        while (
-            self.cluster_state_provider.current_time + pd.Timedelta(minutes=self.sleep_interval_minutes)
-            < self.cluster_state_provider.end_time
-        ):
+        while self.cluster_state_provider.current_time + pd.Timedelta(
+                minutes=self.sleep_interval_minutes) < self.cluster_state_provider.end_time:
 
             # Core simulation logic (with progress tracking)
             self._execute_simulation_step()
@@ -214,7 +187,9 @@ class InMemoryRunnerSimulator:
         self.cluster_state_provider.flush_metrics_data(f"{self.target_simulation_dir}/perf_event_log.csv")
 
     def _execute_simulation_step(self):
-        """This function holds the core simulation logic shared by both run_simulation and run_simulation_with_progress."""
+        """
+        This function holds the core simulation logic shared by both run_simulation and run_simulation_with_progress.
+        """
         # Get the next window of data to simulate
         recorded_data, latest_time = self.cluster_state_provider.get_next_recorded_data()
 
@@ -240,21 +215,20 @@ class InMemoryRunnerSimulator:
 
 
 def main():
-    """TODO: Do we want to remove main here? It might confuse users, but might be useful for testing."""
-    parser = argparse.ArgumentParser(description="InMemoryRunnerSimulator Command Line Interface")
-    parser.add_argument(
-        "--algorithm",
-        choices=["oracle", "multiplicative", "additive"],
-        default="multiplicative",
-        help='Name of the algorithm (e.g., "oracle", "multiplicative")',
-    )
-    parser.add_argument("--data_dir", help="Path to the data directory")
-    parser.add_argument("--config_path", help="Path to the config file")
-    parser.add_argument("--lag", type=int, default=10, help="Lag value (default: 10)")
+    """
+    TODO: Do we want to remove main here? It might confuse users, but might be useful for testing.
+    """
+    parser = argparse.ArgumentParser(description='InMemoryRunnerSimulator Command Line Interface')
+    parser.add_argument('--algorithm', choices=['oracle', 'multiplicative', "additive"], default="multiplicative",
+                        help='Name of the algorithm (e.g., "oracle", "multiplicative")')
+    parser.add_argument('--data_dir', help='Path to the data directory')
+    parser.add_argument('--config_path', help='Path to the config file')
+    parser.add_argument('--lag', type=int, default=10, help='Lag value (default: 10)')
 
     args = parser.parse_args()
 
-    runner = InMemoryRunnerSimulator(data_dir=args.data_dir, algorithm=args.algorithm, config_path=args.config_path)
+    runner = InMemoryRunnerSimulator(data_dir=args.data_dir, algorithm=args.algorithm,
+                                     config_path=args.config_path, lag=args.lag)
     runner.run_simulation()
 
 
